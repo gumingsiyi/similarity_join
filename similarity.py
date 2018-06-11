5da0fc589d44d2f8180831104a964810f3a21ae5
gumingsiyi <stiles1024@qq.com>
2018-06-11 14:30:52 +0800

    修改读取文件路径

--- similarity.py
+++ similarity.py
@@ -2,7 +2,7 @@ from pyspark import SparkContext
 
 
 sc = SparkContext("local","Simple App")
-textfile = sc.textFile("random1.dat")
+textfile = sc.textFile("hdfs://118.25.191.190:9000/similarity/random1.dat")
 
 counts = textfile.flatMap(lambda line: line.split("\n")).map(lambda word: (word, 1)).reduceByKey(lambda a, b: a + b)  
-counts.saveAsTextFile("output.txt")
+counts.saveAsTextFile("output")
